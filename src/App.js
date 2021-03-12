7c908b31cba647801229781a9473550e36e920b1
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-12 17:21:02 -0500

    add tutorial elements & CSS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="microphone-wrapper">
+      <div className="mircophone-container">
+        <div className="microphone-icon-container">
+          <img src={microPhoneIcon} className="microphone-icon" />
+        </div>
+        <div className="microphone-status">
+          Click to start Listening
+        </div>
+        <button className="microphone-stop btn" >
+          Stop
+        </button>
+      </div>
+
+        <div className="microphone-result-container">
+          <div className="microphone-result-text">Speech text here</div>
+          <button className="microphone-reset btn" >
+            Reset
+          </button>
+        </div>
+
     </div>
   );
 }
