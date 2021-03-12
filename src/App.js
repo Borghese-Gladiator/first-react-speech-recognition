494111096285aade6f6e5dae2dae88df94d14b05
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-12 17:22:52 -0500

    use react-speech-recognition (tutorial)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import "./App.css";
+import microPhoneIcon from "./microphone.svg";
 
 function App() {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript();
+  };
   return (
     <div className="microphone-wrapper">
       <div className="mircophone-container">
-        <div className="microphone-icon-container">
+        <div
+          className="microphone-icon-container"
+          ref={microphoneRef}
+          onClick={handleListing}
+        >
           <img src={microPhoneIcon} className="microphone-icon" />
         </div>
         <div className="microphone-status">
-          Click to start Listening
+          {isListening ? "Listening........." : "Click to start Listening"}
         </div>
-        <button className="microphone-stop btn" >
-          Stop
-        </button>
+        {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
       </div>
-
+      {transcript && (
         <div className="microphone-result-container">
-          <div className="microphone-result-text">Speech text here</div>
-          <button className="microphone-reset btn" >
+          <div className="microphone-result-text">{transcript}</div>
+          <button className="microphone-reset btn" onClick={handleReset}>
             Reset
           </button>
         </div>
-
+      )}
     </div>
   );
 }
-
 export default App;
